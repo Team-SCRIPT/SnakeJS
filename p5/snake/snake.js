ed82ed8c425bd4c239c8269a87f34cd34fe9f95a
Akhil Narang <akhilnarang.1999@gmail.com>
2018-11-29 13:26:48 +0530

    SnakeJS: snake: display a alert box!
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- p5/snake/snake.js
+++ p5/snake/snake.js
@@ -6,34 +6,35 @@ function Snake()
 	this.yspeed=0;
 	this.total=1;
 	this.tail=[];
-	
+
 	this.eat=function(pos)
 	{
 		var d=dist(this.x,this.y,pos.x,pos.y);
 		if(d<1){this.total++;return true;}
 		else {return false;}
 	}
-	
+
 	this.dir=function(x,y)
 	{
 		this.xspeed=x;
 		this.yspeed=y;
 	}
-	
+
 	this.death=function(){
-	
+
 		for(var i=0;i<this.tail.length;i++)
 		{
 			var pos=this.tail[i];
 			var d=dist(this.x,this.y,pos.x,pos.y);
 			if(d<1)
 			{
+				window.alert('Starting over! You lose!');
 				console.log('starting over');
 				this.total=0;
 				this.tail=[];
 			}
 		}
-	
+
 	}
 	this.update=function()
 	{
@@ -42,17 +43,17 @@ function Snake()
 			for(var i=0;i<this.tail.length-1;i++)
 			{
 			this.tail[i]=this.tail[i+1];
-			}	
-					
+			}
+
 		}
 		this.tail[this.total-1]=createVector(this.x,this.y);
 		this.x=this.x+this.xspeed*scl;
 		this.y=this.y+this.yspeed*scl;
-		
+
 		this.x=constrain(this.x,0,width-scl);
 		this.y=constrain(this.y,0,height-scl);
 	}
-	
+
 	this.show=function()
 	{
 		fill(255);
@@ -60,7 +61,7 @@ function Snake()
 		{
 			rect(this.tail[i].x,this.tail[i].y,scl,scl);
 		}
-		
+
 		rect(this.x,this.y,scl,scl);
 	}
 }
